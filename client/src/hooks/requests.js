d78093a9010f015dcbe8b994e543589e2f67a84a
Shahzaib Riaz <szaib.975@gmail.com>
2022-08-01 15:53:39 +0500

    add docker and containers

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = "http://localhost:4000/v1";
+const BASE_API_URL = "v1";
 async function httpGetPlanets() {
   const res = await fetch(`${BASE_API_URL}/planets`);
   return await res.json();
